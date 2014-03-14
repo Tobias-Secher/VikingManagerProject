554681c37590397868a10c0ffe45c05126a86c34
FrederikKS <frederik.svendsen@gmail.com>
2014-03-14 10:01:12 +0100

    Removed Gameworld, moved GameLoop to Form

--- VikingManager/VikingManager/Form1.cs
+++ VikingManager/VikingManager/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,16 @@ namespace VikingManager
 {
     public partial class Form1 : Form
     {
-        GameWorld gw;
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
 
+        private DateTime lastFrameStarted = new DateTime();
+        private float currentFPS;
+
+        #region Form1 stuff
         public Form1()
         {
             InitializeComponent();
@@ -30,10 +39,78 @@ namespace VikingManager
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-                if (gw == null)
-                {
-                    gw = new GameWorld();
-                }
+            GameLoop();
         }
+        #endregion
+
+
+        public void SetupWorld()
+        {
+            //Starting FPS timert
+            lastFrameStarted = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Makes sure all the update functions is called every frames
+        /// </summary>
+        public void GameLoop()
+        {
+            //Time spent since last loop
+            TimeSpan deltaTime = DateTime.Now - lastFrameStarted;
+
+            //Convert deltaTime to milliseconds, 1ms minimum
+            int milliSeconds = deltaTime.Milliseconds > 0 ? deltaTime.Milliseconds : 1;
+            currentFPS = 1000 / milliSeconds;
+
+            //Set new frame start
+            lastFrameStarted = DateTime.Now;
+
+            Update();
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void SetConnection()
+        {
+            sql_con = new SQLiteConnection(@"Data Source=C:\Users\Ole\testole.db;Version=3;New=False;Compress=True;");
+        }
+
+        public void LoadData()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select id, desc from mains";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            //   Grid.DataSource = DT;
+            sql_con.Close();
+        }
+
+        public void Add()
+        {
+            string txtSQLQuery = "insert into figur4 values (null,'ole')";
+            //txtSQLQuery = "create table figur4(id integer,navn varchar(4), primary key(id))";
+
+            ExecuteQuery(txtSQLQuery);
+        }
+
+        public void ExecuteQuery(string txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+        
     }
 }
